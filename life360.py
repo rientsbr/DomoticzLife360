a223df2813862fbfbb6584c98fb5c4fc25289825
Rients Brandsma <37273415+rientsbr@users.noreply.github.com>
2024-01-29 21:47:58 +0100

    Aanpassing

--- life360.py
+++ life360.py
@@ -5,13 +5,13 @@ from urllib.error import URLError, HTTPError
 import json
 
 class life360:
-
+    
     base_url = "https://api-cloudfront.life360.com/"
     token_url = "v3/oauth2/token"
     circles_url = "v4/circles"
     circle_url = "circles/"
     user_agent = "com.life360.android.safetymapd/KOKO/23.49.0 android/13"
-    
+
 
     def __init__(self, authorization_token=None, username=None, password=None):
         self.authorization_token = authorization_token
@@ -45,21 +45,21 @@ class life360:
             sleep(600)
 
     def authenticate(self):
+        
+
         url = self.base_url + self.token_url
         params = {
             "grant_type":"password",
-            'username': self.username,
-            'password': self.password
+            "username":self.username,
+            "password":self.password,
         }
+
         r = self.make_request(url=url, params=params, method='POST', authheader="Basic " + self.authorization_token)
-        if r!= 'Error':        
+        try:
             self.access_token = r['access_token']
-            Domoticz.Debug('Token Received: '+self.access_token)
             return True
-        else:
-                Domoticz.Log('No Token Received; Please Check Life360 Username and Password')
-                Domoticz.Debug('You can Validate Your Credentials in www.life360.com')
-                return False            
+        except:
+            return False           
 
     def get_circle_id(self):
         url = self.base_url + self.circles_url
