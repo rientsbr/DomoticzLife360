e29634ea06c61afe81f2f4657b2a66010c79f3d4
Rients Brandsma <37273415+rientsbr@users.noreply.github.com>
2024-01-29 21:34:38 +0100

    Nieuwe Test

--- life360.py
+++ life360.py
@@ -5,12 +5,13 @@ from urllib.error import URLError, HTTPError
 import json
 
 class life360:
-    
-    base_url = "https://api-cloudfront.life360.com/v4/"
-    token_url = "oauth2/token"
-    circles_url = "circles.json"
-    circle_url = "circles"
+
+    base_url = "https://api-cloudfront.life360.com/"
+    token_url = "v3/oauth2/token"
+    circles_url = "v4/circles"
+    circle_url = "circles/"
     user_agent = "com.life360.android.safetymapd/KOKO/23.49.0 android/13"
+    
 
     def __init__(self, authorization_token=None, username=None, password=None):
         self.authorization_token = authorization_token
@@ -23,11 +24,25 @@ class life360:
             headers.update({'Authorization': authheader, 'cache-control': "no-cache",})
         
         if method == 'GET':
-            r = requests.get(url, headers=headers)            
+            try:
+                r = requests.get(url, headers=headers, timeout=30)
+            except requests.exceptions.Timeout:
+                print("Timed out")
+                self.make_request(self, url, params=None, method='GET', authheader=None)
+            
         elif method == 'POST':
-            r = requests.post(url, data=params, headers=headers)
-
-       return r.json()
+            try:
+                r = requests.post(url, data=params, headers=headers, timeout=30)
+            except requests.exceptions.Timeout:
+                print("Timed out")
+                self.make_request(self, url, params=None, method='GET', authheader=None)
+        try:
+            
+            return r.json()
+        except requests.exceptions.JSONDecodeError as E:
+            print(f"There has been an issue connecting to life360's servers! | {E}")
+            from time import sleep
+            sleep(600)
 
     def authenticate(self):
         url = self.base_url + self.token_url
